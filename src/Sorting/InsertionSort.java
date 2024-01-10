f76d22c9421a92af2112afa73f076290371d036e
Sneha Babuji <snehabsivakripa@gmail.com>
2024-01-10 15:58:26 -0500

    Insertion and Merge Sort

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -1,4 +1,30 @@
 package Sorting;
-
 public class InsertionSort {
+
+    public void sort(int[] num){
+
+        for(int j=2;j< num.length;j++){
+            int key=num[j];
+            int i=j-1;
+            while (i>=0 && num[i]>key){
+                num[i+1]=num[i];
+                i--;
+
+            }
+            num[i+1]=key;
+        }
+    }
+ public static void main(String args[]){
+     InsertionSort insertionSort=new InsertionSort();
+     int num[]={2,3,1,5,9};
+     System.out.println("Before Sorting");
+     for(int n: num) {
+         System.out.print(n+" ");
+     }
+     insertionSort.sort(num);
+     System.out.println("\nAfter Sorting");
+     for(int n: num) {
+         System.out.print(n+" ");
+     }
+ }
 }
--- src/Sorting/MergeSort.java
+++ src/Sorting/MergeSort.java
@@ -1,4 +1,60 @@
 package Sorting;
 
 public class MergeSort {
+
+    public void sorting(int[] num, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            sorting(num, start, mid);
+            sorting(num, mid + 1, end);
+            merging(num, start, mid, end);
+        }
+    }
+
+    private void merging(int[] num, int start, int mid, int end) {
+        int[] b = new int[num.length];
+        int i = start;
+        int j = mid + 1;
+        int k = start;
+        while (i <= mid && j <= end) {
+            if (num[i] < num[j]) {
+                b[k] = num[i];
+                i++;
+            } else {
+                b[k] = num[j];
+                j++;
+            }
+            k++;
+        }
+        if (i > mid) {
+            while (j <= end) {
+                b[k] = num[j];
+                k++;
+                j++;
+            }
+        } else if (i <= mid) {
+            while (i <= mid) {
+                b[k] = num[i];
+                k++;
+                i++;
+            }
+        }
+        for (k = start; k <= end; k++) {
+            num[k] = b[k];
+        }
+    }
+
+    public static void main(String args[]) {
+        MergeSort mergeSort = new MergeSort();
+        int num[] = {2, 3, 1, 5, 9};
+        System.out.println("Before Sorting");
+        for (int n : num) {
+            System.out.print(n + " ");
+        }
+        mergeSort.sorting(num, 0, num.length - 1);
+        System.out.println("\nAfter Sorting");
+        for (int n : num) {
+            System.out.print(n + " ");
+        }
+    }
 }
